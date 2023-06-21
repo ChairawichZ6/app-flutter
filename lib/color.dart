c2d52ec16a29b7f85263161b278b2fd469ff7d33
ChairawichZ6 <fluk20540@hotmail.com>
2023-06-21 14:08:19 +0700

    a1
    test

---
+++ lib/color.dart
@@ -0,0 +1,23 @@
+ import 'package:flutter/material.dart';
+
+ const MaterialColor primary = MaterialColor(_primaryPrimaryValue, <int, Color>{
+  50: Color(0xFFFEF1E5),
+  100: Color(0xFFFCDDBD),
+  200: Color(0xFFFAC792),
+  300: Color(0xFFF7B066),
+  400: Color(0xFFF69F45),
+  500: Color(_primaryPrimaryValue),
+  600: Color(0xFFF38620),
+  700: Color(0xFFF17B1B),
+  800: Color(0xFFEF7116),
+  900: Color(0xFFEC5F0D),
+});
+const int _primaryPrimaryValue = 0xFFF48E24;
+
+ const MaterialColor primaryAccent = MaterialColor(_primaryAccentValue, <int, Color>{
+  100: Color(0xFFFFFFFF),
+  200: Color(_primaryAccentValue),
+  400: Color(0xFFFFCAB1),
+  700: Color(0xFFFFB997),
+});
+const int _primaryAccentValue = 0xFFFFEDE4;
---
+++ lib/photo_data.dart
@@ -0,0 +1,13 @@
+import 'package:myapp/photo.dart';
+
+List<Photo> photos = [
+  Photo('https://i.ytimg.com/vi/97hyI5T0Nck/maxresdefault.jpg'),
+  Photo('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdzssMBO6XlLQ2M_6CPchYM-5BfCkkB5VbnDz78I9ul9y3PZhWJAws5iQO67GPbJ9E4j0&usqp=CAU'),
+  Photo('https://www.borntodev.com/wp-content/uploads/2022/05/38033354_1904287879629496_1565701761929314304_n-3.png'),
+  Photo('https://i.ytimg.com/vi/NA2R7zagNzM/maxresdefault.jpg'),
+  Photo('https://www.howtogeek.com/wp-content/uploads/csit/2021/11/af168a3a.jpeg?height=200p&trim=2,2,2,2g'),
+  Photo('https://www.it.kmitl.ac.th/wp-content/themes/itkmitl2017wp/img/life/life-13.jpg'),
+  Photo('https://i.ytimg.com/vi/L2In4h-gPEs/frame0.jpg'),
+  Photo('https://i.ytimg.com/vi/h881BY2vFk8/maxresdefault.jpg'),
+  Photo('https://i.ytimg.com/vi/zeGvSV9-mSI/mqdefault.jpg'),
+];
